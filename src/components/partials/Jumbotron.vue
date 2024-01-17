8ec4eaaf84505da9eca296cab17f48189bc8cdac
estherpinos <estherpinos1@gmail.com>
2024-01-17 19:31:41 +0100

    [fix]:jumbotron

--- src/components/partials/Jumbotron.vue
+++ src/components/partials/Jumbotron.vue
@@ -39,58 +39,24 @@ export default {
 <template>
 
 
-  <div class=" position-relative ">
-=======
-  <div class="jumbotron">
-
-    <div class="content-jumbotron d-flex flex-column justify-content-center ">
-
-      <h2 class=" text-center mb-3 ">Title</h2>
-
-      <form class="d-flex" role="search">
-        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-        <button class="btn btn-outline-success" type="submit">Search</button>
-      </form>
-
-
-    <div class="jumbotron ">
+  <div class="position-relative ">
+     <div class="jumbotron">
         <div class="black-jumbotron"></div>
-    </div>
-
-    
-    <div class="content-jumbotron d-flex flex-column justify-content-center position-absolute top-0 bottom-0 start-50 translate-middle-x ">
-=======
-
-
-        <h2 class=" text-center mb-3 text-white z-3 ">Trova l'appartamento dei tuoi sogni</h2>
-        <form class="d-flex" role="search">
-            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-            <button class="btn btn-success" type="submit">Search</button>
-        </form>
-
-    </div>
-
-
+        <div class="content-jumbotron d-flex flex-column justify-content-center position-absolute top-0 bottom-0 start-50 translate-middle-x ">
+          <h2 class=" text-center mb-3 text-white z-3 ">Trova l'appartamento dei tuoi sogni</h2>
+          <form class="d-flex" role="search">
+              <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+              <button class="btn btn-success" type="submit">Search</button>
+          </form>
+        </div>
+      </div>
   </div>
-
+  
+</template>
  
    
- 
-</template>
-
-<style lang="scss" scoped>
-
-.jumbotron{
-=======
-
-
-  </div>
-</template>
-
 <style lang="scss" scoped>
 .jumbotron {
-  background-color: greenyellow;
-
   height: 400px;
   display: flex;
   justify-content: center;
