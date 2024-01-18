ab5cb37f4afe2cb3e7ab4b7f47f2ce09a24d9f9b
AnotherDebug <antonio.faddanno@gmail.com>
2024-01-18 15:09:19 +0100

    [ADD]: page Error404

---
+++ src/components/pages/Error404.vue
@@ -0,0 +1,31 @@
+<script>
+
+
+export default {
+  name: 'Erroe404',
+  components: {
+  }
+}
+</script>
+
+
+<template>
+  <Header />
+
+  <div class="container d-flex flex-column align-items-center justify-content-center bg-danger">
+    <h1>Error | 404</h1>
+    <h2>PAGE NOT FOUND</h2>
+  </div>
+
+</template>
+
+
+
+<style lang="scss" scoped>
+
+.container {
+
+  height: calc(100vh - 100px);
+}
+
+</style>
